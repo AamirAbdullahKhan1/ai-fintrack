69b6986795a24e835c0561d827be4d87598bdfcf
Aamir Khan <sengoku264@gmail.com>
2024-12-18 14:56:50 +0530

    middleware

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
-import { ClerkProvider } from "@clerk/nextjs/dist/types/components.server";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({subsets:["latin"]})
 
